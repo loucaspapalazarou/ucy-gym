1d019b1b7549c3a1f3efaba1ddbbf6131b315ba8
Loukas Papalazarou <loukas.papalazarou.cy@gmail.com>
2024-04-18 22:14:26 +0300

    using only requests

--- main.py
+++ main.py
@@ -2,28 +2,20 @@ import os
 import sys
 import time
 import logging
-from typing import Any, Callable
-from datetime import datetime, timedelta
-
+import requests
 import schedule
+from datetime import datetime, timedelta
 from dotenv import load_dotenv
-from playwright.sync_api import sync_playwright
 
-# Monday(0) - Saturday(5)
+
 RESERVATION_TIMETABLE = {
     0: ("18:30", "20:00"),
-    1: "18:30",
-    2: "18:30",
-    3: "18:30",
-    4: "18:30",
-    # 5: "10:30",
+    1: ("18:30", "20:00"),
+    2: ("18:30", "20:00"),
+    3: ("18:30", "20:00"),
+    4: ("18:30", "20:00"),
+    # 5: ("08:45", "10:15"),
 }
-
-# login url for alumni
-# different for active students
-LOGIN_URL = (
-    "https://applications2.ucy.ac.cy/pub_sportscenter/sportscenter.main_alumni?p_lang="
-)
 
 logging.basicConfig(
     level=logging.INFO,
@@ -34,14 +26,9 @@ logging.basicConfig(
 )
 
 
-def handle_exception(func):
-    def wrapper(*args, **kwargs):
-        try:
-            return func(*args, **kwargs)
-        except Exception as e:
-            logging.error(f"An error occurred in {func.__name__}: {e}")
-
-    return wrapper
+def get_env_credentials() -> tuple[str, str]:
+    load_dotenv()
+    return os.getenv("GYM_USERNAME", ""), os.getenv("GYM_PASSWORD", "")
 
 
 def date_to_reserve() -> tuple[str, int]:
@@ -49,71 +36,116 @@ def date_to_reserve() -> tuple[str, int]:
     return date.strftime("%d-%m-%Y"), date.weekday()
 
 
-def get_env_credentials() -> tuple[str, str]:
-    load_dotenv()
-    return os.getenv("GYM_USERNAME", ""), os.getenv("GYM_PASSWORD", "")
-
-
-@handle_exception
 def make_reservation(
     username: str,
     password: str,
-    reservation_timetable: dict[int, str],
-    date: str = None,
-    headless: bool = True,
-) -> None:
+    reservation_timetable: dict[int, (str, str)],
+    res_date=None,
+    alumni=True,
+) -> int:
+    headers = {
+        "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7",
+        "Accept-Language": "en-GB,en;q=0.9,el-CY;q=0.8,el;q=0.7,en-US;q=0.6",
+        "Cache-Control": "max-age=0",
+        "Connection": "keep-alive",
+        "Content-Type": "multipart/form-data; boundary=----WebKitFormBoundaryOIsMAiV8CqXisSvG",
+        "Origin": "https://applications2.ucy.ac.cy",
+        "Referer": "https://applications2.ucy.ac.cy/pub_sportscenter/SPORTSCENTER.online_reservations_gym_pck2.insert_reservation2",
+        "Sec-Fetch-Dest": "document",
+        "Sec-Fetch-Mode": "navigate",
+        "Sec-Fetch-Site": "same-origin",
+        "Sec-Fetch-User": "?1",
+        "Upgrade-Insecure-Requests": "1",
+        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/123.0.0.0 Safari/537.36",
+        "sec-ch-ua": '"Google Chrome";v="123", "Not:A-Brand";v="8", "Chromium";v="123"',
+        "sec-ch-ua-mobile": "?0",
+        "sec-ch-ua-platform": '"Windows"',
+    }
 
-    if date is None:
-        date, day = date_to_reserve()
+    index_url = "https://applications2.ucy.ac.cy/pub_sportscenter/sportscenter.main_alumni?p_lang="
+    auth_url = "https://applications2.ucy.ac.cy/pub_sportscenter/sportscenter.alumnis_pck.usr_authenticate"
+    res_url = "https://applications2.ucy.ac.cy/pub_sportscenter/SPORTSCENTER.online_reservations_gym_pck2.insert_reservation3"
+
+    if not alumni:
+        raise NotImplementedError("Non-alumni not supported yet.")
+
+    # login to get cookies
+    session = requests.Session()
+    session.get(index_url)
+    data = {
+        "p_username": username,
+        "p_password": password,
+        "p_lang": "",
+    }
+    response = session.post(auth_url, data=data)
+
+    if res_date is None:
+        res_date, day = date_to_reserve()
     else:
-        day = datetime.strptime(date, "%d-%m-%Y").weekday()
+        day = datetime.strptime(res_date, "%d-%m-%Y").weekday()
 
     if day not in reservation_timetable or day == 6:
-        logging.info("No reservation time set for that day")
+        logging.warning("No reservation time set for that day")
         return
-    res_time: str = reservation_timetable[day]
 
-    logging.info(f"Executing run for {username} for {date} at {res_time}")
+    res_time_start: str = reservation_timetable[day][0]
+    res_time_end: str = reservation_timetable[day][1]
 
-    with sync_playwright() as p:
-        browser = p.chromium.launch(headless=headless)
-        page = browser.new_page()
+    logging.info(
+        f"Executing run for {username}. Target date: {res_date}, target time: {res_time_start}-{res_time_end}"
+    )
 
-        # login
-        page.goto(LOGIN_URL)
-        page.fill('input[name="p_username"]', username)
-        page.fill('input[name="p_password"]', password)
-        page.click('button[type="submit"]')
+    data = (
+        "------WebKitFormBoundaryOIsMAiV8CqXisSvG\r\n"
+        'Content-Disposition: form-data; name="p_system_id"\r\n\r\n'
+        "20438\r\n"
+        "------WebKitFormBoundaryOIsMAiV8CqXisSvG\r\n"
+        'Content-Disposition: form-data; name="p_class_code"\r\n\r\n'
+        "41\r\n"
+        "------WebKitFormBoundaryOIsMAiV8CqXisSvG\r\n"
+        'Content-Disposition: form-data; name="p_sttime"\r\n\r\n'
+        f"{res_time_start}\r\n"
+        "------WebKitFormBoundaryOIsMAiV8CqXisSvG\r\n"
+        'Content-Disposition: form-data; name="p_entime"\r\n\r\n'
+        f"{res_time_end}\r\n"
+        "------WebKitFormBoundaryOIsMAiV8CqXisSvG\r\n"
+        'Content-Disposition: form-data; name="p_cost"\r\n\r\n'
+        ".00\r\n"
+        "------WebKitFormBoundaryOIsMAiV8CqXisSvG\r\n"
+        'Content-Disposition: form-data; name="p_reservation_date"\r\n\r\n'
+        f"{res_date}\r\n"
+        "------WebKitFormBoundaryOIsMAiV8CqXisSvG\r\n"
+        'Content-Disposition: form-data; name="p_skopos_list"\r\n\r\n'
+        "gym\r\n"
+        "------WebKitFormBoundaryOIsMAiV8CqXisSvG\r\n"
+        'Content-Disposition: form-data; name="p_skopos"\r\n\r\n'
+        "\r\n"
+        "------WebKitFormBoundaryOIsMAiV8CqXisSvG\r\n"
+        'Content-Disposition: form-data; name="p_inv_ar"\r\n\r\n'
+        "0\r\n"
+        "------WebKitFormBoundaryOIsMAiV8CqXisSvG\r\n"
+        'Content-Disposition: form-data; name="p_persons"\r\n\r\n'
+        "1\r\n"
+        "------WebKitFormBoundaryOIsMAiV8CqXisSvG\r\n"
+        'Content-Disposition: form-data; name="p_sport"\r\n\r\n'
+        "6\r\n"
+        "------WebKitFormBoundaryOIsMAiV8CqXisSvG\r\n"
+        'Content-Disposition: form-data; name="p_reservation_type"\r\n\r\n'
+        "multible_independent\r\n"
+        "------WebKitFormBoundaryOIsMAiV8CqXisSvG\r\n"
+        'Content-Disposition: form-data; name="p_lang"\r\n\r\n'
+        "el\r\n"
+        "------WebKitFormBoundaryOIsMAiV8CqXisSvG--\r\n"
+    )
 
-        # go to res
-        page.goto(
-            "https://applications2.ucy.ac.cy/pub_sportscenter/online_reservations_pck2.insert_reservation?p_lang="
-        )
-        page.check('input[name="terms_accepted"]')
-        page.select_option('select[name="p_sport"]', value="6")
-        page.click('button[type="submit"]')
+    response = session.post(
+        res_url,
+        headers=headers,
+        data=data,
+    )
 
-        # calendar
-        page.click('button[type="submit"]')
-
-        # set options
-        page.select_option('select[name="p_sttime"]', value=res_time)
-        page.fill('textarea[name="p_skopos"]', "gym")
-        page.evaluate(
-            '(selector) => document.querySelector(selector).removeAttribute("readonly")',
-            'input[name="p_reservation_date"]',
-        )
-        page.fill('input[name="p_reservation_date"]', date)
-
-        page.click('button[type="submit"]')
-        page.click('button[type="submit"]')
-
-        # get result
-        result = page.inner_html("li.prntcontent, p.prntcontent")
-        logging.info(f"Response: {result}")
-
-        # done
-        browser.close()
+    logging.info(f"Response code: {response.status_code}")
+    return response.status_code
 
 
 if __name__ == "__main__":
@@ -139,5 +171,4 @@ if __name__ == "__main__":
             password,
             # date=datetime.today().strftime("%d-%m-%Y"),
             reservation_timetable=RESERVATION_TIMETABLE,
-            headless=True,
         )
